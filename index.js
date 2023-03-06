4a9ede4949eb2bc3eff4743d17385e04c1a3782e
jh871 <jennyh871@gmail.com>
2023-03-06 23:32:21 +0000

    fixed bug with response names

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-const team = require("./src/page-template.js"); //added but not doing anything
+// const team = require("./src/page-template.js"); //added but not doing anything
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 //need to req team from render
@@ -108,13 +108,13 @@ function collectData(){
         const manager = new Manager(managerResponse.name, managerResponse.id, managerResponse.email)
         manager.officeNumber = managerResponse.officeNumber;
         teamArray.push(manager)
-        if (manager.role === 'Engineer'){
+        if (managerResponse.role === 'Engineer'){
             engineerPrompt();
-        } else if (manager.role === 'Intern') {
+        } else if (managerResponse.role === 'Intern') {
             internPrompt();
-        } else {
+        } else if (managerResponse.role === 'Exit') {
             exitPrompt();
-        }
+        };
     });
 }
 
@@ -123,11 +123,11 @@ function engineerPrompt(){
         const engineer = new Engineer(engineerResponse.name, engineerResponse.id, engineerResponse.email)
         engineer.github = engineerResponse.github;
         teamArray.push(engineer)
-        if (engineer.role === 'Engineer'){
+        if (engineerResponse.role === 'Engineer'){
             engineerPrompt();
-        } else if (engineer.role === 'Intern') {
+        } else if (engineerResponse.role === 'Intern') {
             internPrompt();
-        } else {
+        } else if (engineerResponse.role === 'Exit') {
             exitPrompt();
         }
     });
@@ -138,11 +138,11 @@ function internPrompt(){
         const intern = new Intern(internResponse.name, internResponse.id, internResponse.email)
         intern.school = internResponse.school;
         teamArray.push(intern)
-        if (intern.role === 'Engineer'){
+        if (internResponse.role === 'Engineer'){
             engineerPrompt();
-        } else if (intern.role === 'Intern') {
+        } else if (internResponse.role === 'Intern') {
             internPrompt();
-        } else {
+        } else if (internResponse.role === 'Exit')  {
             exitPrompt();
         }
     });
@@ -156,20 +156,26 @@ function exitPrompt(){
         message: 'Press ENTER to be finish'
         }
     ]; 
-    function render(teamArray){
-        return team;
-    };
+    console.log(teamArray);
+    writeHTMLfile();
     //this then runs fs to generate HTML - uses render function
 }
 //
 //calling render "passes in array containing all employee objects"
 //the render function returns html block
+function render(teamArray){
+    return teamHTML;
+};
+
+
 
 
 //THEN fs is used to generate file named 'team.html'
-fs.writeFile('team.html', team, (error) => { //this doesnt work at this time bc what is team - needs string
+function writeHTMLfile(){
+fs.writeFile('team.html', teamHTML, (error) => { //this doesnt work at this time bc what is team - needs string
 error ? console.error(error) : "Check output directory for file"
 })
+}
 // can send file to 'output' folder (create this myself, use outputPath to target)
 
 collectData(); //this kicks off inquirer with manager qs - could rename
